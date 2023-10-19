273ad7e0bf4b6f3f06c0e37575e1ac0ac87c4a26
Rozewicz <rozewijm@rose-hulman.edu>
2023-10-19 18:22:48 -0400

    hi

--- src/JeromeMain.java
+++ src/JeromeMain.java
@@ -1,8 +1,15 @@
 import javax.swing.JFrame;
 
 public class JeromeMain {
-	public static void main(String[] args) {
-		JFrame frame = new JFrame();
+	//public static final Dimension DIMENSIONS = new Dimension();
+		public static final int FRAME_W = 1000;
+		public static final int FRAME_H = 800;
 		
-	}
+		@SuppressWarnings("unused")
+		public static void main(String[] args) {
+			JFrame frame = new JFrame();
+			frame.setSize(FRAME_W,FRAME_H);
+			
+			
+		}
 }
