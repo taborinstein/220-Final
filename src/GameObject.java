8256ccf8a6388d6e3a42b35d04ca75de1e04e35b
rozewijm <rozewijm@RHIT-PW05VX8H.rose-hulman.edu>
2023-10-19 18:42:44 -0400

    add gameobject

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,4 +1,15 @@
 
 public class GameObject {
+	private Sprite sprite;
+	private double x,y,w,h,velX,velY;
+	private boolean collidesWithPlayer;
 	
+	public GameObject(double ex, double why, double width, double height, boolean cWP)
+	{
+		x = ex;
+		y = why;
+		w = width;
+		h = height;
+		collidesWithPlayer = cWP;
+	}
 }
